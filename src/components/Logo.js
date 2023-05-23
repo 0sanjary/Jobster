e465a221a819d8197ecedeba2f595aabf79fd98b
Asif Sanjary <0asifsanjary@gmail.com>
2023-05-23 22:33:02 +0700

    initial commit

---
+++ src/components/Logo.js
@@ -0,0 +1,10 @@
+import logo from '../assets/images/logo.svg';
+import React from 'react';
+
+const Logo = () => {
+  return (
+    <img src={logo} alt='Jobtracker Logo' className='logo' />
+  )
+}
+
+export default Logo;
---
+++ src/components/index.js
@@ -0,0 +1,12 @@
+import Logo from "./Logo";
+import FormRow from "./FormRow";
+import Navbar from "./Navbar";
+import SmallSidebar from "./SmallSidebar";
+import BigSidebar from "./BigSidebar";
+import FormRowSelect from "./FormRowSelect";
+import JobsContainer from "./JobsContainer";
+import SearchContainer from "./SearchContainer";
+import ChartsContainer from "./ChartsContainer";
+import StatsContainer from "./StatsContainer";
+
+export { Logo, FormRow, Navbar, SmallSidebar, BigSidebar, FormRowSelect, JobsContainer, SearchContainer, ChartsContainer, StatsContainer };
